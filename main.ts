d3ae541ce3a8fdd619a9fea0a36181e60821cbd7
maplesapling <148518615+maplesapling@users.noreply.github.com>
2023-10-23 22:48:29 -0400

    Tabs to spaces

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import { Plugin } from 'obsidian';
 
 export default class ObsidianPluginTemplate extends Plugin {
-	async onload() {
-		console.clear();
-		console.log('Hello world');
-	}
+    async onload() {
+        console.clear();
+        console.log('Hello world');
+    }
 }
